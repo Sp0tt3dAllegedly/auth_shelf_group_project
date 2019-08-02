e5e0c5387a6cf110fa8054fcf5847972c829ff8d
Molly Ellison <vonnegutter1@gmail.com>
2019-08-02 09:13:43 -0500

    updated test

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -9,28 +9,81 @@ import {
 import { connect } from 'react-redux';
 
 class List extends Component {
+    state = {
+        newItem: {
+            id: '',
+            description: '',
+            image_url: '',
+        }
+    }
+
+    handleChange = (propertyName, event) => {
+        console.log('event happended')
+        this.setState({
+            newItem: {
+                ...this.state.newItem,
+                [propertyName]: event.target.value,
+            }
+        });
+    }
+
+    addNewItem = event => {
+        event.preventDefault();
+        this.props.dispatch({ type: 'POST_LIST', payload: this.state.newItem })
+        this.setState({
+            newItem: {
+                id: this.state.newItem.id + 1,
+                description: '',
+                image_url: '',
+            }
+        });
+    }
+    
     componentDidMount() {
         this.props.dispatch({ type: 'FETCH_LIST',  })
     }
 
     render() {
+
+        // update inputs below to reflect correct information
         return (
-            <ul>
+            <>
+            
+                <form onSubmit={this.addNewItem}>
+                    <input type='text' placeholder='Description' value={this.state.newItem.description}
+                        onChange={(event) => this.handleChange('description', event)} />
+
+                    <input type='text' placeholder='Image URL' value={this.state.newItem.image_url}
+                        onChange={(event) => this.handleChange('image_url', event)} /> 
+                </form>
+            
+
                 {this.props.reduxState.list.map(item => {
                     return (
-                        <li key={item.id}>
-                            <p>{item.description}<br /><img src={item.image_url} alt="" /></p>
+                        <ul key={item.id}>
+                        <li >
+                            {item.description}<br />
+                            <img src={item.image_url} alt="" />
 
 
-                        </li>    
-                    )
-                })}
-            </ul>)
-                                                                      
-
-}
-}
-
+                        </li> 
+                         </ul> 
+                          
+         )
+     })
+      
+          }
+          </>
+          )
+                         
+           
+           
+           
+                                                                     
+          
+          }
+          }
+          
 const mapStateToProps = reduxState => ({
     reduxState,
 });
